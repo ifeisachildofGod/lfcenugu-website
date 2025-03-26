8cc97f5732d0be1b83bbc93acb12c0dc0f2d8b0a
Marcel <izuobasomtochukwu@gmail.com>
2025-03-26 21:59:53 +0100

    added html,js and cascading codes

---
+++ scripts/package.js
@@ -0,0 +1,58 @@
+const menu = [
+ {
+  item: "Home",
+  link: "#"
+ },
+
+ {
+  item: "About",
+  link: "about.html"
+ },
+
+ {
+  item: "Worship Services",
+  link: "about.html"
+ },
+ {
+  item: "Ministries",
+  link: "about.html"
+ },
+ {
+  item: "Events",
+  link: "about.html"
+ },
+ {
+  item: "Sermons and Teachings",
+  link: "about.html"
+ },
+ {
+  item: "Prayer",
+  link: "about.html"
+ },
+
+ {
+  item:"Contact Us",
+  link:"contact.html"
+ },
+ {
+  item:"Get Involved",
+  link:"contact.html"
+ },
+ {
+  item:"Resources",
+  link:"contact.html"
+ },
+ {
+  item:"Donate",
+  link:"contact.html"
+ },
+ {
+  item:"Testimonies",
+  link:"contact.html"
+ },
+ {
+  item:"FAQs",
+  link:"contact.html"
+ },
+];
+
