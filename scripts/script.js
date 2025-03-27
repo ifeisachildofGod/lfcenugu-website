dbbc2943db895a3b6925b8ef880e5528ad37e3c3
ifeisachildofGod <nwufoifegithub@gmail.com>
2025-03-27 19:02:12 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,25 @@
 
 let menuListHTML = "";
-   menu.forEach((list) => {
-    menuListHTML += ` 
-    <nav class="nav">
-     <div class="menu-toggle" id="menuToggle">
-        <span></span>
-        <span></span>
-        <span></span>
-     </div> <!-- toggle end -->
-  </nav>
-    <div class="menu" id="menu">
-                <ul>
-               <li><a href="${list.link}">${list.item}</a></li>  
-               </ul>
-            </div>
-    `;
 
-  });
-  document.querySelector(".counter").innerHTML = menuListHTML;
+menu.forEach((list) => {
+   menuListHTML += ` 
+   <nav class="nav">
+   <div class="menu-toggle" id="menuToggle">
+      <span></span>
+      <span></span>
+      <span></span>
+   </div> <!-- toggle end -->
+</nav>
+   <div class="menu" id="menu">
+               <ul>
+            <li><a href="${list.link}">${list.item}</a></li>  
+            </ul>
+         </div>
+   `;
+
+});
+
+document.querySelector(".counter").innerHTML = menuListHTML;
 
 
   
